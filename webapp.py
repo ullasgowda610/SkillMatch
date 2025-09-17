593f67a9ce19beee350a46bcbe3f1b49544ca3c1
ullasgowda610 <ullasgowdayr06@gmail.com>
2025-09-17 08:53:26 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,56 +1,58 @@
 import streamlit as st
 from pdfextractor import text_extractor
 import google.generativeai as genai
-import os
+import os 
 
-# Congigure the model
-key=os.getenv('GOOGLE_API_KEY')
-genai.configure(api_key=key)
-model=genai.GenerativeModel('gemini-2.5-flash-lite')
+# Configure trhe model 
+key = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key= key)
+model = genai.GenerativeModel('gemini-2.5-flash-lite')
+resume_text = job_desc = None
 
-resume_text=job_desc=None
+# upload resume 
+st.sidebar.image("https://cdn-icons-png.flaticon.com/512/3135/3135755.png", width=100)
+st.sidebar.markdown(":orange[Upload your *resume below* (pdf only)]")
 
-#Upload resume
-st.sidebar.title(':blue[UPLOAD YOUR RESUME (PDF Only)]')
-file=st.sidebar.file_uploader('Resume',type=['pdf'])
+file = st.sidebar.file_uploader('Resume', type=['pdf'])
 if file:
-    resume_text = text_extractor(file)
-    
-
-# Lets define the main page
-
-st.title(':orange[SKILL MATCH]: :blue[AI Assistant skill Matching Tool]')
-st.markdown('#### This application will match your resume and the job decription.It will create a detailed report on the match')
-tips='''Follow these steps to proceed
-* Upload your resume in the sidebar(PDF Only)
-* Copy and paste the job description below for which you are applying for.
-* click the button and see the magic'''
+        resume_text = text_extractor(file)
+        # st.write(resume_text)
+        
+# let define the main page 
+st.title(":orange[Skill Match] AI Assistant skill matching tool")
+st.caption("Your personal AI career coach — helping you land more interviews 🎯")
+st.markdown("#### This application will match your resume and the job description . It will create a detailed report on the match")
+tips = ''' Follow these steps to proceed:
+1. Upload your resume (pdf only).
+2. Copy and past the job description for which you are applying for.
+3. Click the button and see the magic 
+'''
 
 st.write(tips)
 
-job_desc=st.text_area('copy and paste the job description here(press ctrl+enter to run):',max_chars=10000)
+job_desc=st.text_area("Copy and Paste the Job Description here (press ctrl+enter to run)",max_chars=10000,height=200)
 
-prompt=f'''Assume you are a expert in skill matching and creating profiles.
-Match the following resumes with job decription provided by the user.
-resume={resume_text}
-job description={job_desc}
-
+prompt=f'''
+Assume you are expert in skill matching and creating profiles.
+Match the following resume with the job description provided by the user.
+Resume: {resume_text}
+Job_Description: {job_desc}
 
 Your output should be as follows:
-* Give a brief description of the applicant in 3 to 5 lines.
-* Give a range expected ATS score along with the matching and non matching keywords.
-* Give the chances of getting shortlisted for this position in  percentage.
-* Perform SWOT analysis and discuss each and everything in bullet points
-* Suggest what all improvements can be made in resume in order to get better ATS and increase          percentage  og getting shortlisted.
-* Also create two customized resumes as per the job description provided.
-* prepare one page resume in such a format that can be copied and pasted in word and converted to pdf.
-* Use bullet points and tables wherever necessary.
-* Keep the language professional and easy to understand.'''
+* Give a brief description of the applicant in 3-5 lines.
+* Give a range of expected ATS score along with the matching and non matching keywords.
+* Give the chances of getting sorlisted for this position in percentage.
+* perform SWOT analysis and discuss each and everything in bullets points
+* Suggest what all improvement can be made in resume in order get better ats and increase percentage of getting shortlisted.
+* Also create two customised resumes as per the job description provided and increase percentage of getting shortlisted.
+* prepare one page resume in such a way that can be copied and converted in word and can be converted to pdf.
+* use bullet points and table wherever required 
+'''
 
-
-if job_desc:
-    if resume_text:
-        response=model.generate_content(prompt)
+button = st.button('Click')
+if button:
+    if resume_text and job_desc:
+        response = model.generate_content(prompt)
         st.write(response.text)
     else:
-        st.error('Please upload your resume and job description')
+        st.write("Please Upload your resume")
